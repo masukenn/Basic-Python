5e8d91a3634bfbfc655a79b2ce80a9952dc346a9
masukenn <masuken50092@gmail.com>
2023-03-10 15:43:39 +0900

    'tokemasita'

--- AOJ/07/07_B.py
+++ AOJ/07/07_B.py
@@ -1,2 +1,12 @@
-
+while True:
+    n, x = map(int, input().split())
+    c = 0
+    if n == 0 and x == 0:
+        break
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if i + j + k + 3 == x and i != j != k and i < j < k:
+                    c += 1
+    print(c)
 
